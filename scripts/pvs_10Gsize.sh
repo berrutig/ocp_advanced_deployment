945fd85eed6779d4ad3c209945d21e9dece82063
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-16 17:28:11 +0200

    Update pvs_10Gsize.sh

--- scripts/pvs_10Gsize.sh
+++ scripts/pvs_10Gsize.sh
@@ -1,6 +1,31 @@
-#!/usr/bin/env bash
-
 export GUID=`hostname|awk -F. '{print $2}'`
-export volsize="10Gi"
 
-????
+export volsize="5Gi"
+mkdir /root/pvs
+
+echo 'Now create the persistent volumes 26-20 of 10Gi each as RWM-Retain'
+
+for volume in pv{26..50 ; do
+cat << EOF > /root/pvs/${volume}
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "${volume}"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "${volsize}"
+    },
+    "accessModes": [ "ReadWriteMany" ],
+    "nfs": {
+        "path": "/srv/nfs/user-vols/${volume}",
+        "server": "support1.${GUID}.internal"
+    },
+    "persistentVolumeReclaimPolicy": "Retain"
+  }
+}
+EOF
+
+echo  'Creation of persistent volumes 26-50 of 10Gi completed'
+done;
